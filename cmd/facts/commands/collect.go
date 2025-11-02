59c6c57b80a82e6917617b8fe7265451d986885c
redat00 <reno@redat.me>
2025-11-02 23:10:22 +0100

    feat: add `facts collect` command

---
+++ cmd/facts/commands/collect.go
@@ -0,0 +1,35 @@
+package commands
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/redat00/peekl/pkg/facts"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(collectCmd)
+}
+
+var collectCmd = &cobra.Command{
+	Use:   "collect",
+	Short: "Collect facts about your system",
+	Run: func(cmd *cobra.Command, args []string) {
+		facter := facts.NewFacter()
+		facts, err := facter.Collect()
+		if err != nil {
+			logrus.Fatal(
+				fmt.Sprintf("An error happened while gathering facts : %s", err.Error()),
+			)
+		}
+		factsJson, err := json.Marshal(facts)
+		if err != nil {
+			logrus.Fatal(
+				fmt.Sprintf("An error happened while gathering facts : %s", err.Error()),
+			)
+		}
+		fmt.Println(string(factsJson))
+	},
+}
