828eb3e2278ad63d526f6d260eefe4120dc79963
redat00 <reno@redat.me>
2026-02-19 22:09:33 +0100

    feat: add goreleaser configuration, ci job to build debian packages

--- cmd/server/commands/version.go
+++ cmd/server/commands/version.go
@@ -10,10 +10,16 @@ func init() {
 	rootCmd.AddCommand(versionCmd)
 }
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Obtain information about version and compilation",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("peekl-agent version 0.1.0")
+		fmt.Printf("Name: peekl-server\nVersion: %s\nCommit: %s\nDate: %s\n", version, commit, date)
 	},
 }
