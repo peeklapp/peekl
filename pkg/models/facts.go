823a70db73e47a4a085ebe3b1d4e2601491d8df4
redat00 <reno@redat.me>
2026-04-06 23:28:26 +0200

    fix: remove OsFacts from facts
    This commit removes any OsFacts reference as it's not used anymore and
    has been replaced by LSB facts which allows you to get the same kind of
    informations.

--- pkg/models/facts.go
+++ pkg/models/facts.go
@@ -1,10 +1,5 @@
 package models
 
-type OsFacts struct {
-	Arch   string `json:"arch"`
-	Distro string `json:"distro"`
-}
-
 type PackageFact struct {
 	Name    string `json:"name"`
 	Version string `json:"version"`
@@ -89,7 +84,6 @@ type Disk struct {
 
 type Facts struct {
 	Hostname          string             `json:"hostname"`
-	Os                OsFacts            `json:"os"`
 	Packages          []Package          `json:"packages"`
 	NetworkInterfaces []NetworkInterface `json:"network_interfaces"`
 	Lsb               LsbData            `json:"lsb"`
