959cd8de029a9433da68170d21eb8b2500a65df1
redat00 <reno@redat.me>
2026-07-13 11:13:18 +0200

    chore: rename tests to better reflect separation

--- internal/config/code_test.go
+++ internal/config/code_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLoadNonExistentConfiguration(t *testing.T) {
+func TestLoadNonExistentConfigurationCode(t *testing.T) {
 	_, err := NewCodeConfiguration("testdata/code/i_dont_exist.yml")
 	if err == nil {
 		t.Errorf("Should have returned an error because the file doesn't exist.")
@@ -14,7 +14,7 @@ func TestLoadNonExistentConfiguration(t *testing.T) {
 	assert.Equal(t, "No configuration file found at provided path : testdata/code/i_dont_exist.yml", err.Error())
 }
 
-func TestLoadConfiguration(t *testing.T) {
+func TestLoadConfigurationCode(t *testing.T) {
 	config, err := NewCodeConfiguration("testdata/code/config.yml")
 	if err != nil {
 		t.Errorf("Should not have returned any error : %s", err.Error())
@@ -24,7 +24,7 @@ func TestLoadConfiguration(t *testing.T) {
 	assert.Equal(t, "git@github.com:user/repo.git", config.Repository.Url)
 }
 
-func TestLoadConfigurationWithoutDefaults(t *testing.T) {
+func TestLoadConfigurationWithoutDefaultsCode(t *testing.T) {
 	config, err := NewCodeConfiguration("testdata/code/config_without_defaults.yml")
 	if err != nil {
 		t.Errorf("Shoud not have returned any error : %s", err.Error())
