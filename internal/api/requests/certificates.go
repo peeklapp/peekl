c6c7e88c32c85b3fcfe7566ee2ca5ca2efa71444
redat00 <reno@redat.me>
2026-08-22 13:22:16 +0200

    fix: remove unused 'NodeName' field from query to submit CSR

--- internal/api/requests/certificates.go
+++ internal/api/requests/certificates.go
@@ -1,8 +1,7 @@
 package requests
 
 type SubmitCertificateRequest struct {
-	NodeName string `json:"node_name" validate:"required"`
-	CSR      string `json:"csr" validate:"required"`
+	CSR string `json:"csr" validate:"required"`
 }
 
 type RetrieveSignedCertificate struct {
